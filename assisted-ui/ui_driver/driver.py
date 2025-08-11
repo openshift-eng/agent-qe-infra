cbd7d0a7a4dd6b7f56b9db6a32b3419af91911bc
mhanss <mhans@redhat.com>
2025-08-11 22:40:20 +0200

    Add exception handling

--- assisted-ui/ui_driver/driver.py
+++ assisted-ui/ui_driver/driver.py
@@ -37,41 +37,44 @@ class AgentUiDriver(BrowserInstance):
 
     def run(self):
         self.logger.info("=== Starting assisted-ui automation ===")
-        cluster_details_page = self.app.start(f"http://{self.rendezvous_ip}:3001/")
+        try:
+            cluster_details_page = self.app.start(f"http://{self.rendezvous_ip}:3001/")
 
-        virtualization_page = (cluster_details_page
-                               .type_cluster_name(self.cluster_name)
-                               .type_base_domain(self.base_domain)
-                               .type_pull_secret(self.pull_secret)
-                               .click_next_button())
-
-        host_discovery_page = (virtualization_page
-                               .click_operators_navigation()
-                               .click_virtualization_checkbox()
-                               .click_next_button())
-
-        storage_page = (host_discovery_page
-                        .verify_host_count(3)
-                        .click_next_button())
-
-        networking_details_page = (storage_page
+            virtualization_page = (cluster_details_page
+                                   .type_cluster_name(self.cluster_name)
+                                   .type_base_domain(self.base_domain)
+                                   .type_pull_secret(self.pull_secret)
                                    .click_next_button())
 
-        download_credentials_page = (networking_details_page
-                                     .select_user_managed_networking()
-                                     .click_next_button()
-                                     if self.umn else
-                                     networking_details_page
-                                     .type_api_ip(self.api_ip)
-                                     .type_ingress_ip(self.ingress_ip)
-                                     .click_next_button())
+            host_discovery_page = (virtualization_page
+                                   .click_operators_navigation()
+                                   .click_virtualization_checkbox()
+                                   .click_next_button())
 
-        review_page = (download_credentials_page
-                       .check_confirmation()
-                       .click_download_credentials())
+            storage_page = (host_discovery_page
+                            .verify_host_count(3)
+                            .click_next_button())
 
-        installation_progress = review_page.click_install_cluster()
+            networking_details_page = (storage_page
+                                       .click_next_button())
 
-        installation_progress.verify_text("Installation progress")
+            download_credentials_page = (networking_details_page
+                                         .select_user_managed_networking()
+                                         .click_next_button()
+                                         if self.umn else
+                                         networking_details_page
+                                         .type_api_ip(self.api_ip)
+                                         .type_ingress_ip(self.ingress_ip)
+                                         .click_next_button())
 
-        self.app.stop()
+            review_page = (download_credentials_page
+                           .check_confirmation()
+                           .click_download_credentials())
+
+            installation_progress = review_page.click_install_cluster()
+
+            installation_progress.verify_text("Installation progress")
+        except Exception:
+            self.logger.error(f"Exception occurred during the execution.")
+        finally:
+            self.app.stop()
