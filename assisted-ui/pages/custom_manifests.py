7c8b05ed77aa8a3d8adf7dc605d176ec0174905e
mhanss <mhans@redhat.com>
2026-05-07 11:36:47 +0200

    Add logging and stabilize script

--- assisted-ui/pages/custom_manifests.py
+++ assisted-ui/pages/custom_manifests.py
@@ -1,8 +1,7 @@
-from playwright.sync_api import Page
-
-from base.logger import log_page_activity
 from base.logger import get_logger
+from base.logger import log_page_activity
 from pages.review import Review
+from playwright.sync_api import Page, TimeoutError
 
 
 @log_page_activity
@@ -14,9 +13,10 @@ class CustomManifests:
         self.next_button = page.get_by_role("button", name="Next")
 
     def handle_custom_manifests(self):
-        if self.custom_manifests_heading.count() > 0:
+        try:
+            self.custom_manifests_heading.wait_for(state="visible", timeout=5000)
             self.logger.info("Custom manifests page detected (OCP 4.22+)")
             self.next_button.click()
-        else:
+        except TimeoutError:
             self.logger.info("No Custom manifests page (OCP <4.22), already on review page")
         return Review(self.page)
--- assisted-ui/pages/download_credentials.py
+++ assisted-ui/pages/download_credentials.py
@@ -1,16 +1,18 @@
 import os
 import time
 
-from playwright.sync_api import Download
+from playwright.sync_api import Download, expect
 from playwright.sync_api import Page
 
 from base.logger import log_page_activity
+from base.logger import get_logger
 from pages.custom_manifests import CustomManifests
 
 
 @log_page_activity
 class DownloadCredentials:
     def __init__(self, page: Page) -> None:
+        self.logger = get_logger()
         self.page = page
         self.download_confirmation = page.get_by_role("checkbox", name="I understand that I need to")
         self.download_credentials = page.get_by_test_id("wizard-step-actions").get_by_role("button",
@@ -26,10 +28,10 @@ class DownloadCredentials:
         filename = download.suggested_filename  # it's a property, not a method
         save_path = os.path.join("/tmp", filename)
         download.save_as(save_path)
-        print(f"Downloaded: {save_path}")
+        self.logger.info(f"Downloaded: {save_path}")
 
     def click_download_credentials(self):
         time.sleep(5)
         self.download_credentials.click()
-        time.sleep(10)
+        expect(self.download_confirmation).not_to_be_visible(timeout=90000)
         return CustomManifests(self.page)
